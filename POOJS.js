2d377898a9c7f0631d85f0833d11f7848ce35681
jhonygpa <jhonygpa@gmail.com>
2022-04-25 04:30:12 -0300

    Adicionando POOJS

---
+++ POOJS.js
@@ -0,0 +1,61 @@
+class contaBancaria{
+    constructor(agencia, numero, tipo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+    get saldo(){
+        return this._saldo
+    }
+    set saldo(valor){
+        this._saldo = valor;
+    }
+
+    sacar(valor){
+        if(valor > this._saldo){
+            return "Saldo insuficiente"
+        }
+        this._saldo = this._saldo - valor;
+        return this._saldo;
+    }
+
+    depositar(valor){
+        this.saldo = this._saldo + valor;
+        return this._saldo;
+    }
+}
+
+class ContaCorrente extends contaBancaria { 
+    constructor (agencia, numero,cartaoCredito){
+        super(agencia, numero);
+        this.tipo = 'corrente';
+        this._cartaoCredito = 0;
+    }
+    get cartaoCredito(){
+        return this._cartaoCredito;
+    }
+    set cartaoCredito(valor){
+        this._cartaoCredito = valor;
+    }
+}
+
+class ContaPolpanca extends contaBancaria{
+    constructor (agencia, numero){
+        super(agencia, numero);
+        this.tipo = 'polpanca';
+    } 
+}
+
+class ContaUniversitaria extends contaBancaria {
+    constructor (agencia, numero){
+        super(agencia, numero);
+        this.tipo = 'universitaria';
+    } 
+    sacar(valor){
+        if(valor > 500){
+            return "Operaçao negada!"
+        }
+        this._saldo = this._saldo - valor;
+    }
+}
